546fef90ab860f193e78c1932ecb44e0e6665afe
Daniel Marin <danielmarin998@hotmail.com>
2021-08-25 11:50:08 -0500

    introduccion a promesas

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const sometingWillHappen = () => {
+    return new Promise((resolver, rejet) =>{
+        if (true){
+            resolver('Hey!');
+        }else{
+            reject('Whoops!');
+        }
+    });
+};
+sometingWillHappen()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+const sometingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else{
+            const error = new Error ('Whooops!!');
+            reject(error);
+        }
+    });
+}
+
+sometingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+Promise.all([sometingWillHappen(), sometingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch (err => {
+        console.error(err);
+    });
